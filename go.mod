75346da09c25bb101afd97b93e8b34c06b109146
xaviert52 <duke.chavin@gmail.com>
2026-03-10 09:47:16 -0500

    postgre mig

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.1
 	github.com/swaggo/swag v1.16.6
+	gorm.io/driver/postgres v1.6.0
 	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -40,6 +41,10 @@ require (
 	github.com/go-playground/validator/v10 v10.30.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- internal/infrastructure/db/db.go
+++ internal/infrastructure/db/db.go
@@ -2,17 +2,34 @@ package db
 
 import (
 	"flows/internal/domain"
+	"fmt"
+	"os"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func NewDB() (*gorm.DB, error) {
-	// Using SQLite for standalone demonstration of the Flows module
-	// In production, this should be MySQL or PostgreSQL
-	db, err := gorm.Open(sqlite.Open("flows.db"), &gorm.Config{})
+	var db *gorm.DB
+	var err error
+
+	driver := os.Getenv("DB_DRIVER")
+	dsn := os.Getenv("DB_DSN")
+
+	if driver == "postgres" {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	} else {
+		// Fallback por defecto a SQLite si no hay driver especificado
+		dbPath := os.Getenv("DB_PATH")
+		if dbPath == "" {
+			dbPath = "flows.db"
+		}
+		db, err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	}
+
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to connect database: %w", err)
 	}
 
 	// Auto Migrate the schema
